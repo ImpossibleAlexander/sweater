42e22b0a943a516f6bd20077f5b93a28c76198c6
Alexander <42717240+ImpossibleAlexander@users.noreply.github.com>
2019-11-04 22:28:21 +0200

    New branch with page test

---
+++ sweater-FileUpload/src/test/java/com/example/sweater/MockMvcLoginPageTests.java
@@ -0,0 +1,24 @@
+package com.example.sweater;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class MockMvcExampleTests {
+
+    @Test
+    void exampleTest(@Autowired WebTestClient webClient){
+        webClient.get().uri("/login").exchange().expectStatus().isOk().expectBody(String.class).isEqualTo("login.ftl");
+    }
+}
